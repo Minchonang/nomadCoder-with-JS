ea69e72e460b74293c8a5b867fe00cbebbd06dc4
Minchonang <84889516+Minchonang@users.noreply.github.com>
2021-08-24 17:57:00 +0900

    21. 08. 24.
    create input, submit button

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const h1 = document.querySelector(".hello:first-child h1");
+// const loginForm = document.querySelector('#login-form') // or getElementById
+// getElementById는 'id'를 대상으로 하기 때문에 '#'을 붙일 필요가 없다.
 
-function handleTitleClick() {
-   h1.classList.toggle("clicked")
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+function onLoginClick(event) {
+   event.preventDefault(); // preventDefault() -> 브라우저 기본 기능을 막아줌.
+   console.log(loginInput);
 }
 
-h1.addEventListener("click", handleTitleClick);
-
+loginForm.addEventListener("submit", onLoginClick);
