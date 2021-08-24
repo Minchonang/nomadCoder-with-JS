47520ec3c114d0ded4189bb87be970b09acae52d
Minchonang <84889516+Minchonang@users.noreply.github.com>
2021-08-24 16:32:55 +0900

    21. 08. 24.

---
+++ studying_JS/toggle.js
@@ -0,0 +1,10 @@
+toggle.js
+
+const h1 = document.querySelector(".hello:first-child h1");
+
+function handleTitleClick() {
+   h1.classList.toggle("clicked")
+}
+
+h1.addEventListener("click", handleTitleClick);
+
