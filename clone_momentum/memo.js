205a850d5c5b2cc49a6e7f1419eb966e0d701f9e
Minchonang <84889516+Minchonang@users.noreply.github.com>
2021-08-24 15:35:47 +0900

    studying JS

---
+++ clone_momentum/memo.js
@@ -0,0 +1,22 @@
+// const, let, var
+/* 
+var   -> 옛날 방식
+
+const -> 상수, 변경 불가, 기본적으로 사용하고 변경이 필요할 때 let 사용.
+let   -> 변수, 변경 가능. 다시 언급할 때 'let'을 안 붙여도 됨.
+*/
+
+// boolean
+
+/* 
+true
+false
+null
+undefined
+*/
+
+// array
+
+// object
+
+// function
