7a9a3cafef7b6428d44014ec94a5ecd43dc57516
Minchonang <84889516+Minchonang@users.noreply.github.com>
2021-08-24 15:55:31 +0900

    21. 08. 24.

---
+++ studying_JS/memo.js
@@ -0,0 +1,43 @@
+// const, let, var
+/* 
+var -> 옛날 방식
+
+const -> 상수, 변경 불가, 기본적으로 사용하고 변경이 필요할 때 let 사용.
+let -> 변수, 변경 가능.
+*/
+
+// boolean
+
+/* 
+true
+false
+null
+undefined
+*/
+
+// array
+
+// object
+
+// function
+
+// typeof -> 어떤 type 인지 판별.
+
+// parseInt -> 숫자로 변환.
+
+// and, or
+/*
+&& -> and
+|| -> or 
+
+true && true === true
+true && false === false
+false && true === false
+false && false === false
+
+true || true === true
+true || false === true
+false || false === true
+false || false === false
+
+*/
